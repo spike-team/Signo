9c4bd5b6fe28ab720a7cc8bd46bb2f092395e3c9
Jaehoon Kim <by09115@outlook.kr>
2019-09-06 13:47:16 +0900

    ADD: ApplicantTest 생성

---
+++ src/test/java/kim/jaehoon/studentable/signo/SignoApplicationTests.java
@@ -0,0 +1,16 @@
+package kim.jaehoon.studentable.signo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SignoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
