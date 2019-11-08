d83f36f4721eeea4f609a0f4cca56868c64d263c
Jaehoon Kim <by09115@outlook.kr>
2019-11-08 18:51:43 +0900

    ADD: repositories for entities

--- src/main/java/kim/jaehoon/studentable/signo/domain/repository/SubjectRepository.java
+++ src/main/java/kim/jaehoon/studentable/signo/domain/repository/SubjectRepository.java
@@ -1,6 +1,6 @@
 package kim.jaehoon.studentable.signo.domain.repository;
 
-import kim.jaehoon.studentable.signo.domain.entity.Subject;
+import kim.jaehoon.studentable.signo.domain.document.Subject;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
