0f928971fdfd552b31f16c8a9004f54f13928f23
Jaehoon Kim <by09115@outlook.kr>
2019-11-09 16:27:45 +0900

    FIX: return type of service

--- src/main/java/kim/jaehoon/studentable/signo/service/meal/MealServiceImpl.java
+++ src/main/java/kim/jaehoon/studentable/signo/service/meal/MealServiceImpl.java
@@ -1,16 +1,23 @@
 package kim.jaehoon.studentable.signo.service.meal;
 
+import kim.jaehoon.studentable.signo.domain.document.Meal;
+import kim.jaehoon.studentable.signo.domain.document.SchoolInfo;
 import kim.jaehoon.studentable.signo.domain.payload.Menu;
+import kim.jaehoon.studentable.signo.domain.repository.MealRepository;
 import kim.jaehoon.studentable.signo.domain.repository.SchoolInfoRepository;
 import kim.jaehoon.studentable.signo.exception.SchoolError;
+import kim.jaehoon.studentable.signo.exception.SchoolNotFoundException;
 import kr.go.neis.api.School;
 import kr.go.neis.api.SchoolException;
 import kr.go.neis.api.SchoolMenu;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static kim.jaehoon.studentable.signo.util.RegionConverter.toEnum;
 
@@ -18,23 +25,35 @@ import static kim.jaehoon.studentable.signo.util.RegionConverter.toEnum;
 public class MealServiceImpl implements MealService {
 
     @Autowired
-    SchoolInfoRepository schoolRepository;
+    private SchoolInfoRepository schoolRepository;
+
+    @Autowired
+    private MealRepository mealRepository;
 
     @Override
-    public Mono<Menu> findBySchoolAndDate(String schoolCode, LocalDate date) {
-       return schoolRepository.findBySchoolCode(schoolCode)
-                .flatMap(schoolInfo1 -> {
-                    try {
-                        School school = School.find(toEnum(schoolInfo1.getFullName()), schoolInfo1.getName());
+    public Mono<Meal> findBySchoolAndDate(String schoolCode, LocalDate date) {
 
-                        SchoolMenu menu = school
-                                .getMonthlyMenu(date.getYear(), date.getMonthValue())
-                                .get(date.getDayOfMonth() - 1);
+        return schoolRepository.findBySchoolCode(schoolCode)
+                .switchIfEmpty(Mono.error(new SchoolNotFoundException()))
+                .flatMap(schoolInfo -> mealRepository.findBySchoolCodeAndDate(schoolCode, date.toString())
+                        .switchIfEmpty(cashingMeal(schoolCode, schoolInfo, date)));
+    }
 
-                        return Mono.just(new Menu().fromSchoolMenu(menu));
-                    } catch (SchoolException exception) {
-                        throw new SchoolError(exception.getMessage());
-                    }
-                });
+    private Mono<Meal> cashingMeal(String schoolCode, SchoolInfo schoolInfo, LocalDate date) {
+        try {
+            List<SchoolMenu> responses = School.find(toEnum(schoolInfo.getFullName()), schoolInfo.getName())
+                    .getMonthlyMenu(date.getYear(), date.getMonthValue());
+
+            List<Menu> willCashed = new ArrayList<>();
+
+            for (SchoolMenu menu : responses) {
+                willCashed.add(new Menu(menu.breakfast, menu.lunch, menu.dinner));
+            }
+
+            Meal meal = new Meal(new ObjectId(), schoolCode, date.toString(), willCashed);
+            return mealRepository.save(meal).then(Mono.just(meal));
+        } catch (SchoolException exception) {
+            throw new SchoolError(exception.getMessage());
+        }
     }
 }
