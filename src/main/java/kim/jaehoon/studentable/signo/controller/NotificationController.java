47af939a4d6d10a98b583c9ffa44c9276c868288
Jaehoon Kim <by09115@outlook.kr>
2019-12-10 09:11:04 +0900

    FIX: controller of notification, timetable

--- src/main/java/kim/jaehoon/studentable/signo/controller/NotificationController.java
+++ src/main/java/kim/jaehoon/studentable/signo/controller/NotificationController.java
@@ -9,15 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@RequestMapping("/api/v1")
 public class NotificationController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
--- src/main/java/kim/jaehoon/studentable/signo/controller/TimetableController.java
+++ src/main/java/kim/jaehoon/studentable/signo/controller/TimetableController.java
@@ -1,38 +1,70 @@
 package kim.jaehoon.studentable.signo.controller;
 
+import kim.jaehoon.studentable.signo.domain.entity.Changes;
 import kim.jaehoon.studentable.signo.domain.entity.Timetable;
-import kim.jaehoon.studentable.signo.domain.payload.MappedTimetable;
-import kim.jaehoon.studentable.signo.domain.repository.TimetableRepository;
+import kim.jaehoon.studentable.signo.domain.payload.PreChanges;
+import kim.jaehoon.studentable.signo.service.changes.ChangesService;
 import kim.jaehoon.studentable.signo.service.timetable.TimetableService;
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
+import kim.jaehoon.studentable.signo.service.token.TokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
+import java.io.IOException;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1")
 public class TimetableController {
 
+    private TokenService tokenService;
+    private ChangesService changesService;
     private TimetableService timetableService;
-    private TimetableRepository timetableRepository;
 
-    @Autowired
-    public TimetableController(TimetableService timetableService, TimetableRepository timetableRepository) {
-        this.timetableService = timetableService;
-        this.timetableRepository = timetableRepository;
+    @GetMapping(value = "/timetable")
+    public Mono<Timetable> getBySchoolClass(@RequestParam String schoolCode, @RequestParam String schoolClass) {
+        return timetableService.findBySchoolCodeAndSchoolClass(schoolCode, schoolClass);
     }
 
-    @GetMapping("/timetables")
+    @GetMapping(value = "/timetable/excel")
+    public Mono generateExcelSheetForSingleTimetable(ServerHttpResponse response, @RequestParam String schoolCode, @RequestParam String schoolClass) {
+        return timetableService.generateExcelSheetForSingleTimetable(response, schoolCode, schoolClass);
+    }
+
+    @PutMapping(value = "/timetable/excel", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Mono importSingleTimetable(@RequestHeader("Authorization") String auth, @RequestPart("excelData") FilePart files, @RequestParam("schoolClass") String schoolClass) {
+        String managerEmail = tokenService.getIdentity(auth.replace("Bearer ", ""));
+        return timetableService.importExcelSheetForSingleTimetable(managerEmail, schoolClass, files);
+    }
+
+    @GetMapping(value = "/timetables")
     public Flux<Timetable> getAll() {
         return timetableService.findAll();
     }
 
-    @GetMapping("/timetable")
-    public Mono<MappedTimetable> getBySchoolClass(@RequestParam String schoolCode, @RequestParam("schoolClass") String schoolClass) {
-        return timetableService.findBySchoolCodeAndSchoolClass(schoolCode, schoolClass);
+    @GetMapping(value = "/timetables/excel")
+    public Mono generateExcelSheetForMultipleTimetable(ServerHttpResponse response, @RequestParam String schoolCode) throws IOException {
+        return timetableService.generateExcelSheetForMultipleTimetable(response, schoolCode);
+    }
+
+    @PutMapping(value = "/timetables/excel", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Flux importMultipleTimetable(@RequestHeader("Authorization") String auth, @RequestPart("excelData") FilePart files) {
+        String managerEmail = tokenService.getIdentity(auth.replace("Bearer ", ""));
+        return timetableService.importExcelSheetForMultipleTimetable(managerEmail, files);
+    }
+
+    @GetMapping(value = "/timetable/changes")
+    public Flux<Changes> getChanges(@RequestParam String schoolCode, @RequestParam String schoolClass) {
+        return changesService.findAll(schoolCode, schoolClass);
+    }
+
+    @PostMapping(value = "/timetable/changes")
+    public Mono<Changes> appendChanges(@RequestBody PreChanges prechanges) {
+        return changesService.appendChanges(prechanges.toChanges());
     }
 
 }
